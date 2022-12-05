587c192610788e2aa5b2b9eeac9c70a8afb452da
sabest2022 <askarians@me.com>
2022-12-05 06:11:27 +0100

    dfv

---
+++ ShoppingCart/CartItem.php
@@ -0,0 +1,42 @@
+<?php
+
+
+class CartItem
+{
+    private $product;
+    private $quantity;
+
+    // TODO Skriv en konstruktor som sätter alla properties
+
+    // TODO Skriv getters för alla properties
+    public function __construct($product, $quantity)
+    {
+        $this->product = $product;
+        $this->quantity = $quantity;
+        return [$product, $quantity];
+    }
+    public function getProduct()
+    {
+        return $this->product;
+    }
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    //VG: Skall utöka antalet på ett cartItem med 1. 
+    //VG: Det skall inte vara möjligt att utöka så att antalet överstiger produktens $inStock.
+    public function increaseQuantity()
+    {
+        if ($this->getQuantity() === $this->product->getInstock()) {
+            echo $this->product->getTitle() . "   Kan inte läggas till. Slut i lager";
+            return;
+        } else {
+            $this->quantity += 1;
+        }
+    }
+    public function decreaseQuantity()
+    {
+        $this->quantity -= 1;
+    }
+}
